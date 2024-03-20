3946b0ee8f34f80833c227ee38c6a257dfddc0fc
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-03-20 15:43:38 +0100

    funzione genera numero casuali

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,10 +1,53 @@
 const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', () => {
-    console.log("crash");
+    play();
+
 })
 
 // Visualizzare in pagina 5 numeri casuali ( tra 1 e 100) non duplicati.
 // Da lì parte un timer di 30 secondi.
-// Dopo i 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt() ( o meglio caselle di input).
+// Dopo i 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, 
+//tramite il prompt() ( o meglio caselle di input).
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+//funzione del gioco
+function play() {
+    getRandomNumber(1, 100)
+}
+
+//funzione per generare i numeri casuali
+function getRandomNumber(numMin, numMax) {
+    //console.log("crash");
+    //array vuoto del numeri casuali
+    const numberRandom = [];
+    console.log(numberRandom);
+    const numeroMaxGenerate = 5;
+    //condizione mentre la linghezza dell'array è minore del numero di numeri da generare
+    while (numberRandom.length < numeroMaxGenerate) {
+        //genera un numero casuale
+        let number = parseInt(Math.random() * numMax - numMin);
+        // controlla se non è nell'array e lo pusha
+        if (!numberRandom.includes(number)) {
+            numberRandom.push(number);
+        }
+    }
+    console.log(numberRandom);
+
+
+}
+//funzione per disegnare i quadrati
+function draw(maxSquare, numeroPrint) {
+    // crea l'elemento html 
+    // assegna la classe
+    //innerHTML del numero
+
+    // prendi l'elemento html
+    //appendi i quadrati 
+
+}
+// ciclare i numeri e metterli dentro ai quadrati
+//funzione settimeout
+//funzione mostra caselle di input e pulsante submit
+// crea un nuovo array e confrontalo con l'array del pc
+// restituisci risultato
